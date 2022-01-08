7bb99719f0951edfe2ba24bcd3a46fd24183cd89
mohamed saied <62272115+mohamedsaied87613@users.noreply.github.com>
2022-01-08 21:43:50 +0200

    Add files via upload

--- addcar.php
+++ addcar.php
@@ -13,20 +13,20 @@
         
     $sql = "SELECT * FROM car WHERE car_plate_id='$plate_id'";
 
-if ($result = $db->query($sql))
-{
-    $row = $result->fetch_assoc();
-    if ($row) {
-        echo "<h1>Plate_Id Already Exists!</h1><br>";
-        echo '<a href="addcar.html">Back</a>';
+    if ($result = $db->query($sql))
+    {
+        $row = $result->fetch_assoc();
+        if ($row) {
+             echo "<h1>Plate_Id Already Exists!</h1><br>";
+             echo '<a href="addcar.html">Back</a>';
 
-    } else {
-        $sql = "INSERT INTO car (car_plate_id , car_manufacture , car_model,car_year,car_status,distance_covered,daily_rent,office) VALUES ('$plate_id','$car_manufacture','$car_model','$model_year','$car_status','$distance_covered','$daily_rent','$office')";
-        $db->query($sql);
-		echo "<h1>Car ADDED!</h1><br>";
-        echo '<a href="addcar.html">Back</a>';        
-    }
-} 
+        } else {
+            $sql = "INSERT INTO car (car_plate_id , car_manufacture , car_model,car_year,car_status,distance_covered,daily_rent,office) VALUES ('$plate_id','$car_manufacture','$car_model','$model_year','$car_status','$distance_covered','$daily_rent','$office')";
+            $db->query($sql);
+		    echo "<h1>Car ADDED!</h1><br>";
+            echo '<a href="admin_panel.html">Back</a>';        
+        }
+    } 
 else {
     echo "<h1>Failed to addcar!</h1><br>";
     echo "Error: " . $sql . "<br>" . $db->error;
